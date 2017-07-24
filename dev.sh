78c299d000fbaf0cd6eea883f997ebc5df9cf2ab
Richard Denton <isdampe@gmail.com>
2017-07-24 10:53:28 +1000

    Auto kill previous web server

--- dev.sh
+++ dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+proc=`ps aux | grep node | grep http | awk 'BEGIN{FS=" "} {print $2}'`
+pkill $proc
 gulp sass js
 node ./node_modules/http-server/bin/http-server -p 8001 &> /dev/null &
 echo "Listening on http://localhost:8001/"
