28ee387dc0443f9d3bfc1c5c7cac535bbb26419d
Richard Denton <isdampe@gmail.com>
2017-07-24 11:00:35 +1000

    Fixes dev.sh

--- dev.sh
+++ dev.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-proc=`ps aux | grep node | grep http | awk 'BEGIN{FS=" "} {print $2}'`
-pkill $proc
-gulp sass js
+function killServer() {
+	proc=`ps aux | grep node | grep http | awk 'BEGIN{FS=" "} {print $2}'`
+	if [ -z "$proc" ]
+	then
+		echo "No dev http-server is active. Ignoring."
+	else
+		echo "Closing dev http-server"
+		kill $proc
+	fi
+}
+
+killServer
 node ./node_modules/http-server/bin/http-server -p 8001 &> /dev/null &
 echo "Listening on http://localhost:8001/"
+gulp sass js
 gulp
+killServer
